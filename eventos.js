18fc4fe0a69fb78fb95d39b74d71a69287ddad04
PaulaSuaFl <paulasuafl@gmail.com>
2024-09-01 01:34:17 -0300

    arreglo click

--- eventos.js
+++ eventos.js
@@ -1,1 +1,13 @@
 document.getElementById("saludar").addEventListener("click",()=>{alert("Hola, Soy el div")})
+    const boton = document.getElementById('saludar');
+    const div = document.querySelector('.divboton');
+    
+
+    boton.addEventListener('click', (event) => {
+        event.stopPropagation(); 
+        alert('Botón clickeado');
+    });
+
+    div.addEventListener('click', () => {
+        alert('Div clickeado');
+    });
